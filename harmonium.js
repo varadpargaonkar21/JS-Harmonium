649b2e30b092663aefa1cda34f3e860688e461d0
Varad Pargaonkar <117361138+varadpargaonkar21@users.noreply.github.com>
2022-12-08 23:13:02 +0530

    Add files via upload

---
+++ harmonium.js
@@ -0,0 +1,357 @@
+function C(){
+     document.getElementById("C").value = "SAA";
+     document.getElementById("D").value = "RE";
+     document.getElementById("DD").value = "RE";
+     document.getElementById("E").value = "GA";
+     document.getElementById("EE").value = "GA";
+     document.getElementById("F").value = "MA";
+     document.getElementById("FF").value = "MA";
+     document.getElementById("G").value = "PA";
+     document.getElementById("GG").value = "PA";
+     document.getElementById("A").value = "DHA";
+     document.getElementById("AA").value = "DHA";
+     document.getElementById("B").value = "NI";
+     document.getElementById("BB").value = "NI";
+     document.getElementById("CC").value = "SAA";
+     document.getElementById("C1").value = "K-RE";
+     document.getElementById("CC1").value = "K-RE";
+     document.getElementById("D1").value = "K-GA";
+     document.getElementById("DD1").value = "K-GA";
+     document.getElementById("F1").value = "T-MA";
+     document.getElementById("FF1").value = "T-MA";
+     document.getElementById("G1").value = "K-DHA";
+     document.getElementById("GG1").value = "K-DHA";
+     document.getElementById("A1").value = "K-NI";
+     document.getElementById("AA1").value = "K-NI";
+}
+
+function C1(){
+
+    
+    document.getElementById("C1").value = "SAA";
+    document.getElementById("D").value = "K-RE";
+    document.getElementById("DD").value = "K-RE";
+    document.getElementById("D1").value = "RE";
+    document.getElementById("DD1").value = "RE";
+    document.getElementById("E").value = "K-GA";
+    document.getElementById("EE").value = "K-GA";
+    document.getElementById("F").value = "GA";
+    document.getElementById("FF").value = "GA";
+    document.getElementById("F1").value = "MA";
+    document.getElementById("FF1").value = "MA";
+    document.getElementById("G").value = "T-MA";
+    document.getElementById("GG").value = "T-MA";
+    document.getElementById("G1").value = "PA";
+    document.getElementById("GG1").value = "PA";
+    document.getElementById("A").value = "K-DHA";
+    document.getElementById("AA").value = "K-DHA";
+    document.getElementById("A1").value = "DHA";
+    document.getElementById("AA1").value = "DHA";
+    document.getElementById("B").value = "K-NI";
+    document.getElementById("BB").value = "K-NI";
+    document.getElementById("CC").value = "NI";
+    document.getElementById("C").value = "NI";
+    document.getElementById("CC1").value = "SAA";
+    
+}
+
+function D(){
+
+    
+    document.getElementById("D").value = "SAA";
+    document.getElementById("D1").value = "K-RE";
+    document.getElementById("DD1").value = "K-RE";
+    document.getElementById("E").value = "RE";
+    document.getElementById("EE").value = "RE";
+    document.getElementById("F").value = "K-GA";
+    document.getElementById("FF").value = "K-GA";
+    document.getElementById("F1").value = "GA";
+    document.getElementById("FF1").value = "GA";
+    document.getElementById("G").value = "MA";
+    document.getElementById("GG").value = "MA";
+    document.getElementById("G1").value = "T-MA";
+    document.getElementById("GG1").value = "T-MA";
+    document.getElementById("A").value = "PA";
+    document.getElementById("AA").value = "PA";
+    document.getElementById("A1").value = "K-DHA";
+    document.getElementById("AA1").value = "K-DHA";
+    document.getElementById("B").value = "DHA";
+    document.getElementById("BB").value = "DHA";
+    document.getElementById("CC").value = "K-NI";
+    document.getElementById("C").value = "K-NI";
+    document.getElementById("CC1").value = "NI";
+    document.getElementById("C1").value = "NI";
+    document.getElementById("DD").value = "SAA";
+    }
+
+
+
+function D1(){
+
+    document.getElementById("D1").value = "SAA";
+    document.getElementById("E").value = "K-RE";
+    document.getElementById("EE").value = "K-RE";
+    document.getElementById("F").value = "RE";
+    document.getElementById("FF").value = "RE";
+    document.getElementById("F1").value = "K-GA";
+    document.getElementById("FF1").value = "K-GA";
+    document.getElementById("G").value = "GA";
+    document.getElementById("GG").value = "GA";
+    document.getElementById("G1").value = "MA";
+    document.getElementById("GG1").value = "MA";
+    document.getElementById("A").value = "T-MA";
+    document.getElementById("AA").value = "T-MA";
+    document.getElementById("A1").value = "PA";
+    document.getElementById("AA1").value = "PA";
+    document.getElementById("B").value = "K-DHA";
+    document.getElementById("BB").value = "K-DHA";
+    document.getElementById("CC").value = "DHA";
+    document.getElementById("C").value = "DHA";
+    document.getElementById("CC1").value = "K-NI";
+    document.getElementById("C1").value = "K-NI";
+    document.getElementById("DD").value = "NI";
+    document.getElementById("D").value = "NI";
+    document.getElementById("DD1").value = "SAA";
+}
+
+
+
+function E(){
+
+    document.getElementById("E").value = "SAA";
+    document.getElementById("F").value = "K-RE";
+    document.getElementById("FF").value = "K-RE";
+    document.getElementById("F1").value = "RE";
+    document.getElementById("FF1").value = "RE";
+    document.getElementById("G").value = "K-GA";
+    document.getElementById("GG").value = "K-GA";
+    document.getElementById("G1").value = "GA";
+    document.getElementById("GG1").value = "GA";
+    document.getElementById("A").value = "MA";
+    document.getElementById("AA").value = "MA";
+    document.getElementById("A1").value = "T-MA";
+    document.getElementById("AA1").value = "T-MA";
+    document.getElementById("B").value = "PA";
+    document.getElementById("BB").value = "PA";
+    document.getElementById("CC").value = "K-DHA";
+    document.getElementById("C").value = "K-DHA";
+    document.getElementById("CC1").value = "DHA";
+    document.getElementById("C1").value = "DHA";
+    document.getElementById("DD").value = "K-NI";
+    document.getElementById("D").value = "K-NI";
+    document.getElementById("DD1").value = "NI";
+    document.getElementById("D1").value = "NI";
+    document.getElementById("EE").value = "SAA";
+    
+}
+
+
+function F(){
+
+    document.getElementById("F").value = "SAA";
+    document.getElementById("F1").value = "K-RE";
+    document.getElementById("FF1").value = "K-RE";
+    document.getElementById("G").value = "RE";
+    document.getElementById("GG").value = "RE";
+    document.getElementById("G1").value = "K-GA";
+    document.getElementById("GG1").value = "K-GA";
+    document.getElementById("A").value = "GA";
+    document.getElementById("AA").value = "GA";
+    document.getElementById("A1").value = "MA";
+    document.getElementById("AA1").value = "MA";
+    document.getElementById("B").value = "T-MA";
+    document.getElementById("BB").value = "T-MA";
+    document.getElementById("CC").value = "PA";
+    document.getElementById("C").value = "PA";
+    document.getElementById("CC1").value = "K-DHA";
+    document.getElementById("C1").value = "K-DHA";
+    document.getElementById("DD").value = "DHA";
+    document.getElementById("D").value = "DHA";
+    document.getElementById("DD1").value = "K-NI";
+    document.getElementById("D1").value = "K-NI";
+    document.getElementById("EE").value = "NI";
+    document.getElementById("E").value = "NI";
+    document.getElementById("FF").value = "SAA";
+    
+}
+
+
+function F1(){
+
+    document.getElementById("F1").value = "SAA";
+    document.getElementById("G").value = "K-RE";
+    document.getElementById("GG").value = "K-RE";
+    document.getElementById("G1").value = "RE";
+    document.getElementById("GG1").value = "RE";
+    document.getElementById("A").value = "K-GA";
+    document.getElementById("AA").value = "K-GA";
+    document.getElementById("A1").value = "GA";
+    document.getElementById("AA1").value = "GA";
+    document.getElementById("B").value = "MA";
+    document.getElementById("BB").value = "MA";
+    document.getElementById("CC").value = "T-MA";
+    document.getElementById("C").value = "T-MA";
+    document.getElementById("CC1").value = "PA";
+    document.getElementById("C1").value = "PA";
+    document.getElementById("DD").value = "K-DHA";
+    document.getElementById("D").value = "K-DHA";
+    document.getElementById("DD1").value = "DHA";
+    document.getElementById("D1").value = "DHA";
+    document.getElementById("EE").value = "K-NI";
+    document.getElementById("E").value = "K-NI";
+    document.getElementById("FF").value = "NI";
+    document.getElementById("F").value = "NI";
+    document.getElementById("FF1").value = "SAA";
+}
+
+
+function G(){
+
+    
+    document.getElementById("G").value = "SAA";
+    document.getElementById("G1").value = "K-RE";
+    document.getElementById("GG1").value = "K-RE";
+    document.getElementById("A").value = "RE";
+    document.getElementById("AA").value = "RE";
+    document.getElementById("A1").value = "K-GA";
+    document.getElementById("AA1").value = "K-GA";
+    document.getElementById("B").value = "GA";
+    document.getElementById("BB").value = "GA";
+    document.getElementById("CC").value = "MA";
+    document.getElementById("C").value = "MA";
+    document.getElementById("CC1").value = "T-MA";
+    document.getElementById("C1").value = "T-MA";
+    document.getElementById("DD").value = "PA";
+    document.getElementById("D").value = "PA";
+    document.getElementById("DD1").value = "K-DHA";
+    document.getElementById("D1").value = "K-DHA";
+    document.getElementById("EE").value = "DHA";
+    document.getElementById("E").value = "DHA";
+    document.getElementById("FF").value = "K-NI";
+    document.getElementById("F").value = "K-NI";
+    document.getElementById("FF1").value = "NI";
+    document.getElementById("F1").value = "NI";
+    document.getElementById("GG").value = "SAA";
+    
+}
+
+
+function G1(){
+
+    document.getElementById("G1").value = "SAA";
+    document.getElementById("A").value = "K-RE";
+    document.getElementById("AA").value = "K-RE";
+    document.getElementById("A1").value = "RE";
+    document.getElementById("AA1").value = "RE";
+    document.getElementById("B").value = "K-GA";
+    document.getElementById("BB").value = "K-GA";
+    document.getElementById("CC").value = "GA";
+    document.getElementById("C").value = "GA";
+    document.getElementById("CC1").value = "MA";
+    document.getElementById("C1").value = "MA";
+    document.getElementById("DD").value = "T-MA";
+    document.getElementById("D").value = "T-MA";
+    document.getElementById("DD1").value = "PA";
+    document.getElementById("D1").value = "PA";
+    document.getElementById("EE").value = "K-DHA";
+    document.getElementById("E").value = "K-DHA";
+    document.getElementById("FF").value = "DHA";
+    document.getElementById("F").value = "DHA";
+    document.getElementById("FF1").value = "K-NI";
+    document.getElementById("F1").value = "K-NI";
+    document.getElementById("GG").value = "NI";
+    document.getElementById("G").value = "NI";
+    document.getElementById("GG1").value = "SAA";
+    
+}
+
+
+function A(){
+
+    document.getElementById("A").value = "SAA";
+    document.getElementById("A1").value = "K-RE";
+    document.getElementById("AA1").value = "K-RE";
+    document.getElementById("B").value = "RE";
+    document.getElementById("BB").value = "RE";
+    document.getElementById("CC").value = "K-GA";
+    document.getElementById("C").value = "K-GA";
+    document.getElementById("CC1").value = "GA";
+    document.getElementById("C1").value = "GA";
+    document.getElementById("DD").value = "MA";
+    document.getElementById("D").value = "MA";
+    document.getElementById("DD1").value = "T-MA";
+    document.getElementById("D1").value = "T-MA";
+    document.getElementById("EE").value = "PA";
+    document.getElementById("E").value = "PA";
+    document.getElementById("FF").value = "K-DHA";
+    document.getElementById("F").value = "K-DHA";
+    document.getElementById("FF1").value = "DHA";
+    document.getElementById("F1").value = "DHA";
+    document.getElementById("GG").value = "K-NI";
+    document.getElementById("G").value = "K-NI";
+    document.getElementById("GG1").value = "NI";
+    document.getElementById("G1").value = "NI";
+    document.getElementById("AA").value = "SAA";
+    
+}
+
+
+
+function A1(){
+
+    document.getElementById("A1").value = "SAA";
+    document.getElementById("B").value = "K-RE";
+    document.getElementById("BB").value = "K-RE";
+    document.getElementById("CC").value = "RE";
+    document.getElementById("C").value = "RE";
+    document.getElementById("CC1").value = "K-GA";
+    document.getElementById("C1").value = "K-GA";
+    document.getElementById("DD").value = "GA";
+    document.getElementById("D").value = "GA";
+    document.getElementById("DD1").value = "MA";
+    document.getElementById("D1").value = "MA";
+    document.getElementById("EE").value = "T-MA";
+    document.getElementById("E").value = "T-MA";
+    document.getElementById("FF").value = "PA";
+    document.getElementById("F").value = "PA";
+    document.getElementById("FF1").value = "K-DHA";
+    document.getElementById("F1").value = "K-DHA";
+    document.getElementById("GG").value = "DHA";
+    document.getElementById("G").value = "DHA";
+    document.getElementById("GG1").value = "K-NI";
+    document.getElementById("G1").value = "K-NI";
+    document.getElementById("AA").value = "NI";
+    document.getElementById("A").value = "NI";
+    document.getElementById("AA1").value = "SAA";
+    
+}
+
+
+function B(){
+
+    document.getElementById("B").value = "SAA";
+    document.getElementById("CC").value = "K-RE";
+    document.getElementById("C").value = "K-RE";
+    document.getElementById("CC1").value = "RE";
+    document.getElementById("C1").value = "RE";
+    document.getElementById("DD").value = "K-GA";
+    document.getElementById("D").value = "K-GA";
+    document.getElementById("DD1").value = "GA";
+    document.getElementById("D1").value = "GA";
+    document.getElementById("EE").value = "MA";
+    document.getElementById("E").value = "MA";
+    document.getElementById("FF").value = "T-MA";
+    document.getElementById("F").value = "T-MA";
+    document.getElementById("FF1").value = "PA";
+    document.getElementById("F1").value = "PA";
+    document.getElementById("GG").value = "K-DHA";
+    document.getElementById("G").value = "K-DHA";
+    document.getElementById("GG1").value = "DHA";
+    document.getElementById("G1").value = "DHA";
+    document.getElementById("AA").value = "K-NI";
+    document.getElementById("A").value = "K-NI";
+    document.getElementById("AA1").value = "NI";
+    document.getElementById("A1").value = "NI";
+    document.getElementById("BB").value = "SAA";
+    
+}
